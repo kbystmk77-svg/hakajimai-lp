ec7906628a8e3969c65fb87b5b5045e15b07592c
kbystmk77-svg <kbystmk77@gmail.com>
2026-03-04 17:10:33 +0900

    Create ThanksPage component for thank you message

---
+++ app/thanks/page.tsx
@@ -0,0 +1,8 @@
+export default function ThanksPage() {
+  return (
+    <main style={{ maxWidth: 720, margin: "80px auto", textAlign: "center" }}>
+      <h1>送信ありがとうございました</h1>
+      <p>担当者より折り返しご連絡いたします。</p>
+    </main>
+  )
+}
